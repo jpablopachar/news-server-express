663a3837fedb6ed89bf8fbcb8830f5b688021e1b
jpablopachar <jpablopachar1993@gmail.com>
2024-12-12 10:32:29 -0500

    Se agregan funciones para eliminar y actualizar perfiles de usuario, así como para cambiar contraseñas y obtener información del perfil.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
-import { compare, hash } from 'bcrypt'
+import { compare, genSalt, hash } from 'bcrypt'
 import { v2 as cloudinary } from 'cloudinary'
+import formidable from 'formidable'
 import { sign } from 'jsonwebtoken'
 import {
   CLOUD_API_KEY,
@@ -150,6 +151,139 @@ export class AuthController {
       })
     } catch (error) {
       console.error('Error in updateWriter', error)
+
+      return responseReturn(res, 500, { error: 'Internal server error' })
+    }
+  }
+
+  deleteWriter = async (req, res) => {
+    const { id } = req.params
+
+    try {
+      const writer = await Auth.findByIdAndDelete(id)
+
+      if (!writer)
+        return responseReturn(res, 404, { error: 'Writer not found' })
+
+      return responseReturn(res, 200, {
+        message: 'Writer deleted successfully',
+      })
+    } catch (error) {
+      console.error('Error in deleteWriter', error)
+
+      return responseReturn(res, 500, { error: 'Internal server error' })
+    }
+  }
+
+  updateProfile = async (req, res) => {
+    const form = formidable({ multiples: true })
+
+    form.parse(req, async (err, fields, files) => {
+      if (err)
+        return responseReturn(res, 400, {
+          message: 'Image upload failed',
+          error: err,
+        })
+
+      try {
+        const name = Array.isArray(fields.name) ? fields.name[0] : fields.name
+        const email = Array.isArray(fields.email)
+          ? fields.email[0]
+          : fields.email
+
+        if (!name)
+          return responseReturn(res, 400, { error: 'Name is required' })
+
+        if (!email)
+          return responseReturn(res, 400, { error: 'Email is required' })
+
+        let updateData = { name: name.trim(), email: ElementInternals.trim() }
+
+        const uploadedImage = Array.isArray(files.image)
+          ? files.image[0]
+          : files.image
+
+        if (uploadedImage && uploadedImage.filepath) {
+          const { url } = await cloudinary.uploader.upload(
+            uploadedImage.filepath,
+            { folder: 'news_images' }
+          )
+
+          updateData.image = url
+        } else {
+          console.log('No image uploaded')
+        }
+
+        const updateUser = await Auth.findByIdAndUpdate(
+          req.params.id,
+          updateData,
+          { new: true }
+        )
+
+        return responseReturn(res, 200, {
+          message: 'Profile updated successfully',
+          user: updateUser,
+        })
+      } catch (error) {
+        console.error('Error in updateProfile', error)
+
+        return responseReturn(res, 500, { error: 'Internal server error' })
+      }
+    })
+  }
+
+  getProfile = async (req, res) => {
+    const { id } = req.params
+
+    if (!id) return responseReturn(res, 400, { error: 'User Id required' })
+
+    try {
+      const user = await Auth.findById(id)
+
+      if (!user) return responseReturn(res, 404, { error: 'User not found' })
+
+      return responseReturn(res, 200, { user })
+    } catch (error) {
+      console.error('Error in getProfile', error)
+
+      return responseReturn(res, 500, { error: 'Internal server error' })
+    }
+  }
+
+  changePassword = async (req, res) => {
+    const { oldPassword, newPassword } = req.body
+
+    const { id } = req.params
+
+    if (!oldPassword)
+      return responseReturn(res, 400, { error: 'Old password required' })
+
+    if (!newPassword)
+      return responseReturn(res, 400, { error: 'New password required' })
+
+    try {
+      const user = await Auth.findById(id).select('+password')
+
+      if (!user) return responseReturn(res, 404, { error: 'User not found' })
+
+      const passwordMatch = await compare(oldPassword, user.password)
+
+      if (!passwordMatch)
+        return responseReturn(res, 401, { error: 'Invalid old password' })
+
+      const salt = await genSalt(10)
+
+      const hashedPassword = await hash(newPassword, salt)
+
+      user.password = hashedPassword
+
+      await user.save()
+
+      return responseReturn(res, 200, {
+        message: 'Password changed successfully',
+      })
+    } catch (error) {
+      console.error('Error in changePassword', error)
 
       return responseReturn(res, 500, { error: 'Internal server error' })
     }
